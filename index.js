ee06652f4db68aad20f8b7ca08dad68393c3a5eb
JaleelaBasheer <jaleelabasheer91@gmail.com>
2024-07-31 16:50:21 +0530

    podeselectronjsonserver

---
+++ index.js
@@ -0,0 +1,25 @@
+// 1. Import json-server
+const jsonServer = require('json-server');
+const path = require('path');
+const fs = require('fs');
+
+// 2. Create server app
+const server = jsonServer.create();
+
+// 3. Generate a Middleware to use in json server
+const middleware = jsonServer.defaults();
+
+// 4. Set up route/path for json file
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+
+// 5. Set up port number to run server
+const port = process.env.PORT || 4000;
+
+// 6. Use middleware and router to the server
+server.use(middleware);
+server.use(router);
+
+// 7. To listen/run server in specified port
+server.listen(port, () => {
+    console.log(`JSON server is running at http://localhost:${port}`);
+});
