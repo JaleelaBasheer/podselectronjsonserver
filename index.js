6cce2031cb551d648acd5320221abdc410eae187
JaleelaBasheer <jaleelabasheer91@gmail.com>
2024-08-07 12:06:42 +0530

    update

--- index.js
+++ index.js
@@ -1,27 +1,25 @@
-// 1. Import json-server
-const jsonServer = require('json-server');
-const path = require('path');
-const fs = require('fs');
-const nodemailer = require('nodemailer');
-const crypto = require('crypto');
+// Steps to generate json server
+
+//1. import json-server
+const jsonServer = require('json-server')
 
 // 2. Create server app
-const server = jsonServer.create();
+const server = jsonServer.create()
 
 // 3. Generate a Middleware to use in json server
-const middleware = jsonServer.defaults();
+const middleware = jsonServer.defaults()
 
-// 4. Set up route/path for json file
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
+// 4. Set uo route/path for json file
+const router = jsonServer.router("db.json")
 
 // 5. Set up port number to run server
-const port = process.env.PORT || 4000;
+const port = 3000 || process.env.PORT
 
-// 6. Use middleware and router to the server
-server.use(middleware);
-server.use(router);
+// 6. use middleware and router to the server
+server.use(middleware)
+server.use(router)
 
 // 7. To listen/run server in specified port
-server.listen(port, () => {
-    console.log(`JSON server is running at http://localhost:${port}`);
-});
+server.listen(port,()=>{
+    console.log(`Contact server app started at port ${port}`);
+})
